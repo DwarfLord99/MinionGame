c943576db00c4cd5ff006525863800008af0f5c3
DwarfLord99 <golem.rock@yahoo.com>
2025-12-13 15:37:42 -0500

    Minion Movement Started
    Minion follows player at a specific range. Currently minion can be moved using the right analog stick on gamepad and once stopped moving will return to player. When within the follow range, minion will no longer move until follow range is exceeded.

--- MinionGame/Assets/Project/Scripts/MinionController.cs
+++ MinionGame/Assets/Project/Scripts/MinionController.cs
@@ -1,16 +1,63 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MinionController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Minion Stats")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float followRange = 5f;
+
+    [Header("Minion Components")]
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform playerTransform;
+
+    private InputAction minionSweep;
+    private InputAction minionSend;
+    private InputAction minionRecall;
+
+    private bool isSweeping = false;
+
     void Start()
     {
-        
+        minionSweep = InputSystem.actions.FindAction("Sweep");
+        minionSend = InputSystem.actions.FindAction("Send");
+        minionRecall = InputSystem.actions.FindAction("Recall");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculate distance between minion and player within a specified range
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+
+        // Check if player is within follow range
+        if (distanceToPlayer > followRange && !isSweeping)
+        {
+            // Move towards player
+            transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+        }
+        else if (distanceToPlayer <= followRange && !isSweeping)
+        {
+            // Stop moving when within range
+            rb.linearVelocity = Vector3.zero;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(minionSweep.inProgress)
+            HandleSweep();
+        else
+            isSweeping = false;
+    }
+
+    private void HandleSweep()
+    {
+        Vector2 inputVector = minionSweep.ReadValue<Vector2>();
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y).normalized;
+        Vector3 velocity = moveDirection * moveSpeed;
+
+        rb.linearVelocity = velocity;
+        isSweeping = true;
     }
 }
